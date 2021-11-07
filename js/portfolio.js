5e7d1fa1fe91b246fcd1a91e41ade66a5790e6d1
Nikita <obodzinskiy.nikita@gmail.com>
2021-11-07 18:11:49 +0200

    2.0

---
+++ js/portfolio.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('button[type="logic_button"]').onclick = function() {
+    document.querySelectorAll('button[type="logic_button"]').classList.add('active_logic_button');
+    document.querySelectorAll('button[type="logic_button"]').classList.remove('non_active_logic_button');
+}
+
